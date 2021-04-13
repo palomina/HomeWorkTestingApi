cb3d302af1bf3c18b6001a2582e5f96ee52f5212
OMorozova <palomina.1983@gmail.com>
2021-04-13 23:45:00 +0300

    Homework 3

---
+++ src/test/java/com/imgur/api/BaseTest.java
@@ -0,0 +1,31 @@
+package com.imgur.api;
+
+import groovy.util.logging.Slf4j;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public abstract class BaseTest {
+    protected static Map<String, String> headers = new HashMap<>();
+    protected static String username;
+    protected static String token;
+    static Properties properties = new Properties();
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        loadProperties();
+        RestAssured.baseURI = properties.getProperty("base.url");
+        headers.put("Authorization", properties.getProperty("token"));
+        username = properties.getProperty("username");
+    }
+
+    private static void loadProperties() throws IOException {
+        properties.load(new FileInputStream("src/test/resources/application.properties"));
+    }
+
+}
